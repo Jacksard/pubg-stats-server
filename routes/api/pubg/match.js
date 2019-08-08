1dd3784c9dd81fe6afac4e9f2b73970f6e17a149
Jacksard <jacobgoskha@gmail.com>
2019-08-08 10:53:17 -0400

    Match Call

--- routes/api/pubg/match.js
+++ routes/api/pubg/match.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const data = require('./exports');
 
 // @route   GET api/pubg/player
 // @desc    Return the Initial data
 // @access  Public with client
-router.get('/', (req, res) => res.send('Match API route'));
+router.get('/:matchId', async (req, res) => {
+  const { matchId } = req.params;
+  try {
+    const response = await axios
+      .get(data.url.match + `${matchId}`, {
+        headers: {
+          Authorization: 'Bearer ' + process.env.API_KEY,
+          Accept: 'application/vnd.api+json'
+        }
+      })
+      .then(response => {
+        console.log(response.data);
+        return response.data;
+      });
+    res.json(response);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
