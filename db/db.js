75ddac1656b1697739d3b44cc70fd79ecac343a2
Jacob Goskha <jacobgoskha@gmail.com>
2019-09-26 13:12:58 -0400

    test heroku

--- db/db.js
+++ db/db.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
-    try {
-
-        await mongoose.connect(process.env.MONGODB, {
-            useNewUrlParser: true,
-            useCreateIndex: true
-        });
-        console.log("MongoDB connected...")
-    } catch (err) {
-        console.log(err.message)
-        // Exit process with failure
-        process.exit(1);
-    }
-}
+  try {
+    console.log(process.env.API_KEY);
+    console.log(process.env.MONGODB);
+    await mongoose.connect(process.env.MONGODB, {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.log(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
